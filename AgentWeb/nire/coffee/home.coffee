537e4fd223e8e3dfe036cb2265eb60df00c49e15
Rolland Walsh <rollandwalsh@gmail.com>
2016-06-24 09:43:45 -0700

    adds initial files for nire

---
+++ AgentWeb/nire/coffee/home.coffee
@@ -0,0 +1,16 @@
+bgRotation = Math.floor Math.random() * 3 + 1
+$('body').addClass 'bg-' + bgRotation
+
+$ ->
+	qs = setInterval((->
+		if $('.rn-qsr').length > 0
+			$('.rn-qsr-cz-f').attr('placeholder', 'Search by Address, City, Zip Code, or MLS #').removeAttr 'style'
+			$('.rn-qsr-mn-f option').eq(0).html '$ Min'
+			$('.rn-qsr-mx-f option').eq(0).html '$ Max'
+			$('.rn-qsr-bd-f option').eq(0).html 'Beds'
+			$('.rn-qsr-ba-f option').eq(0).html 'Baths'
+			$('.rn-qsr-button').wrap '<div class="rn-qsr-button-container"></div>'
+			clearInterval qs
+		return
+	), 200)
+	return
---
+++ AgentWeb/nire/js/home.js
@@ -0,0 +1,20 @@
+var bgRotation;
+
+bgRotation = Math.floor(Math.random() * 3 + 1);
+
+$('body').addClass('bg-' + bgRotation);
+
+$(function() {
+  var qs;
+  qs = setInterval((function() {
+    if ($('.rn-qsr').length > 0) {
+      $('.rn-qsr-cz-f').attr('placeholder', 'Search by Address, City, Zip Code, or MLS #').removeAttr('style');
+      $('.rn-qsr-mn-f option').eq(0).html('$ Min');
+      $('.rn-qsr-mx-f option').eq(0).html('$ Max');
+      $('.rn-qsr-bd-f option').eq(0).html('Beds');
+      $('.rn-qsr-ba-f option').eq(0).html('Baths');
+      $('.rn-qsr-button').wrap('<div class="rn-qsr-button-container"></div>');
+      clearInterval(qs);
+    }
+  }), 200);
+});
