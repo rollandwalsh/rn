dfd3191ba60682314511bd3c760e50ccd28d8c09
Rolland Walsh <rollandwalsh@gmail.com>
2016-07-01 09:50:42 -0700

    includes styling / scripts for hmre in source controle

---
+++ AgentWeb/hmre/js/014-home.js
@@ -0,0 +1,35 @@
+var agentContact, agentContactList, carousel, randomSlide;
+
+randomSlide = Math.floor(Math.random() * $('.rn-home-carousel > div').length);
+
+carousel = $('.rn-home-carousel');
+
+carousel.slick({
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 10001,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  slickGoTo: randomSlide
+});
+
+agentContact = $('#rnSiteHeaderAgentContactImg');
+
+agentContactList = $('#rnSiteHeaderAgentContactList');
+
+agentContact.on('click', function() {
+  return agentContactList.slideToggle();
+});
+
+$(function() {
+  var qs;
+  qs = setInterval((function() {
+    if ($('.rn-qsr').length > 0) {
+      $('.rn-qsr-cz-f').attr('placeholder', 'Enter Location or MLS#').removeAttr('style');
+      $('.rn-qsr-button').replaceWith('<button onclick="submitForm()" class="rn-qsr-button" type="submit"><i class="rn-icon-search"></i></button>');
+      clearInterval(qs);
+    }
+  }), 200);
+});
