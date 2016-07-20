60c8bb512fdfff248fdfa35a52fdf9cacbcf1f8e
Rolland Walsh <rollandwalsh@gmail.com>
2016-07-20 10:45:42 -0700

    Initial commit for BH Foster

---
+++ AgentWeb/bhfo/js/home.js
@@ -0,0 +1,22 @@
+var randomSlide;
+
+randomSlide = Math.floor(Math.random() * 4) + 1;
+
+$('.rn-home-page').attr('style', 'background-image: url(img/home-bg-' + randomSlide + '.jpg)');
+
+$(function() {
+  var qs;
+  qs = setInterval((function() {
+    if ($('.rn-qsr').length > 0) {
+      $('.rn-qsr-cz-f').attr('placeholder', 'Enter City, Zip, Address or MLS Numbe').removeAttr('style');
+      $('.rn-qsr-mn-f option').eq(0).html('Min');
+      $('.rn-qsr-mx-f option').eq(0).html('Max');
+      $('.rn-qsr-button').replaceWith('<button onclick="submitForm()" class="rn-qsr-button" type="submit"><i class="rn-icon-search"></i></button>');
+      $('<div class="rn-qsr-selects"></div>').appendTo($('.rn-qsr form'));
+      $('.rn-qsr-mn').appendTo('.rn-qsr-selects');
+      $('.rn-qsr-mx').appendTo('.rn-qsr-selects');
+      $('.rn-qsr-clearctr').remove();
+      clearInterval(qs);
+    }
+  }), 200);
+});
